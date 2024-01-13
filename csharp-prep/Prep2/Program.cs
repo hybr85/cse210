8806c31d3660f3c6f18bf04af7517851ab0edbac
Hyrum B <hyrumb85@gmail.com>
2024-01-13 15:38:47 -0700

    finished assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grades = "FDCBA";
+        
+        Console.Write("What is your grade percentage? ");
+        int percent = int.Parse(Console.ReadLine());
+        int idx = Math.Clamp(percent / 10 - 5, 0, 4);
+
+        Console.WriteLine($"{grades[idx]}{(percent>93||percent<60?null:(percent%10>=7?'+':(percent%10<3?'-':null)))}");
+        Console.WriteLine(percent>=70?"Yay you passed!":"You can do it next time!");
     }
 }
