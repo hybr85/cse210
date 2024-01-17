f5a19adbb70ac41e50564c137cb613ccf8d81da1
Hyrum B <hyrumb85@gmail.com>
2024-01-17 11:11:20 -0700

    finished assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new();
+        int num;
+
+        Console.WriteLine("Enter numbers, 0 when finished.");
+        do {
+            Console.Write("Enter number > ");
+            num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+        } while(num != 0);
+        
+        numbers.Remove(0);
+        Console.WriteLine($"Sum: {numbers.Sum()}, avg: {numbers.Average()}, max: {numbers.Max()}");
+        numbers.Sort();
+        foreach (int i in numbers) Console.Write($"{i} ");
+        Console.WriteLine();
     }
 }
