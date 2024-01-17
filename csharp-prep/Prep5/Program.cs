976403c6cde696a04a783fcd04cab6edbc1d15f5
Hyrum B <hyrumb85@gmail.com>
2024-01-17 12:18:42 -0700

    finished assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name > ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number > ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int x)
+    {
+        return x*x;
+    }
+    static void DisplayResult(string name, int num)
+    {
+        Console.WriteLine($"{name}, your number squared is {num}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
     }
 }
