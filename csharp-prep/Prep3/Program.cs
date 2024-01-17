85c7410049d6c9ca45d2d083124ac114c241616a
Hyrum B <hyrumb85@gmail.com>
2024-01-17 10:49:47 -0700

    finished assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100);
+        int guess;
+        int numGuesses = 0;
+        
+        do {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            numGuesses++;
+
+            if(guess>random) Console.WriteLine("Lower");
+            if(guess<random) Console.WriteLine("Higher");
+        } while(guess!=random);
+        Console.WriteLine($"You guessed it!\nIt took you {numGuesses} guesses");
     }
 }
